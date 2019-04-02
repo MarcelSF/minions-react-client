e6a11f4a52711038f1757d66222cb4c32ef3de09
marcel fonseca <marcelsf23br@gmail.com>
2019-04-02 17:05:56 -0300

    started login and logout

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "minions-inventos-teste"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://e7isavy93l.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_r5k0eKMkA",
+    APP_CLIENT_ID: "50dll5k4h48kvefsddbu13as8f",
+    IDENTITY_POOL_ID: "us-east-1:d5df9b5e-dfe3-4fcc-972b-f52d065c47c6"
+  }
+};
