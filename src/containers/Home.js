31cae9c3b44ab9a59e2f55e657720dde606b91dc
marcel fonseca <marcelsf23br@gmail.com>
2019-04-02 10:30:23 -0300

    initial styling and routes

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Minions!</h1>
+          <p>Get every type of minion you want!</p>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/containers/NotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () =>
+  <div className="NotFound">
+    <h3>Sorry, page not found!</h3>
+    <img class="not_found" src="https://data.whicdn.com/images/204513142/large.jpg"/>
+  </div>
